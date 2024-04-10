591cd186072bae06e6b43cdfd18b381cb31a7a17
smorkgl <catosrak@gmail.com>
2024-04-10 16:52:53 +0300

    123
    123

--- index.js
+++ index.js
@@ -22,9 +22,6 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
-    if (!fs.existsSync("uploads")) {
-      fs.mkdirSync("uploads");
-    }
     cb(null, "uploads");
   },
   filename: (_, file, cb) => {
